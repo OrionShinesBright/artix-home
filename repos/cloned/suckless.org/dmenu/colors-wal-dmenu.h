e77106c86b0e2326ac033de73719c832dfe54448
OrionShinesBright <rafay880333@gmail.com>
2025-10-17 10:44:03 +0500

    Dmenu path changes

---
+++ repos/cloned/suckless.org/dmenu/colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#caa9c1", "#010101" },
+	[SchemeSel] = { "#caa9c1", "#CC6C2A" },
+	[SchemeOut] = { "#caa9c1", "#93B06F" },
+};
