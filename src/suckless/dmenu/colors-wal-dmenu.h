d8efdf235d56e5d863bd30dec04762939afe47fc
OrionShinesBright <rafay880333@gmail.com>
2025-10-17 10:48:21 +0500

    Cleaning up older suckless paths in src

--- src/suckless/dmenu/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c8cb9a", "#11130f" },
-	[SchemeSel] = { "#c8cb9a", "#5C6B4B" },
-	[SchemeOut] = { "#c8cb9a", "#9B9D5D" },
-};
